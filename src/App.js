166f233caa163e3ea1168a593387f26a38c30917
Teo <teo.bostiog@yahoo.com>
2023-04-05 17:42:20 +0300

    trrsf

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "./App.css";
 const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
 
 const App = () => {
-
+console.log('asd')
   const [searchTerm, setSearchTerm] = useState("");
   const [movies, setMovies] = useState([]);
 
