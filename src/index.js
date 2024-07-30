eacb4b14c7003f9f5af76e0136b659e8a915aece
Teo <teo.bostiog@yahoo.com>
2024-07-30 17:56:46 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import Interview from "./Interview";
+import InterviewChatGPT from "./InterviewChatGPT";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // console.log("asds");
